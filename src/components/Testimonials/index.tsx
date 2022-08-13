31d9dde81ef78663b9a8865a60f0c7c032f5b975
Mateus Gouvea <mateuscg42@gmail.com>
2022-08-12 21:03:58 -0300

    add testemonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -9,39 +9,41 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import { Crown } from 'phosphor-react';
+import { ReactNode } from 'react';
+
+import jl from '../../assets/ganhadores/jl.png';
+import jf from '../../assets/ganhadores/jf.png';
+import mc from '../../assets/ganhadores/mc.png';
+import md from '../../assets/ganhadores/md.png';
 
 const testimonials = [
   {
-    name: 'Brandon P.',
-    role: 'Chief Marketing Officer',
+    name: 'Júlio Cesar',
+    role: 'Prêmio Principal',
     content:
       'It really saves me time and effort. It is exactly what our business has been lacking. EEZY is the most valuable business resource we have EVER purchased. After using EEZY my business skyrocketed!',
-    avatar:
-      'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    avatar: `${jl}`,
   },
   {
-    name: 'Krysta B.',
-    role: 'Entrepreneur',
+    name: 'João Felipe',
+    role: 'Prêmio mensal',
     content:
-      "I didn't even need training. We've used EEZY for the last five years. I have gotten at least 50 times the value from EEZY. I made back the purchase price in just 48 hours!",
-    avatar:
-      'https://images.unsplash.com/photo-1598550874175-4d0ef436c909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+      'Participei na promoção Viaje com o Rei em 2021 e, para minha surpresa, ganhei o primeiro sorteio dos prêmios mensais! Gostei tanto que decidi continuar participando em 2022 também.',
+    avatar: `${jf}`,
   },
   {
-    name: 'Darcy L.',
-    role: 'Movie star',
+    name: 'Marilda Bezerra',
+    role: 'Prêmio mensal',
     content:
       "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
-    avatar:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80',
+    avatar: `${md}`,
   },
   {
-    name: 'Daniel T.',
-    role: 'Musician',
+    name: 'Maurício Corrêa',
+    role: 'Prêmio mensal',
     content:
       'I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!',
-    avatar:
-      'https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    avatar: `${mc}`,
   },
 ];
 
