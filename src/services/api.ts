d3e12624c05d41823331d161515f0004e1e0b224
mateus <cast.mateus@pe.sebrae.com.br>
2023-03-31 20:33:54 -0300

    add subdomain

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,8 @@ let baseURL = '';
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3001';
 } else {
-  baseURL = 'http://89.116.214.181:3333';
+  baseURL = 'https://boasorte.viajecomrei.com.br';
+  // baseURL = 'http://89.116.214.181:3333';
 }
 
 export const api = axios.create({
